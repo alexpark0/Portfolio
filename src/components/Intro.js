000e53e09eb8c448cda27def6ac3c7d5fa4818f4
alexpark0 <park.ale@northeastern.edu>
2025-01-02 01:57:02 -0500

    music and colors

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,11 +12,19 @@ function Intro() {
             ></img>
         </div>
         <div className="Bio">
-            <h1>Hi, I'm Alex Park!</h1>
+            <h1>Hi, I'm Alex!</h1>
             <h2>Welcome to my portfolio website!</h2>
-            <h3>I'm an aspiring software engineer pursuing a Bachelor of Science in 
-                Computer Science at Northeastern University. I'm currently in my second year
-                at Northeastern, interning for Revvity during the summer. </h3>
+            <h3>I'm an aspiring software engineer studying Computer Science at Northeastern University. 
+                My main expertise lies in frontend web development and AI/ML. Outside of coding, I love
+                playing basketball, cubing, playing strategy games, playing piano, 
+                and discovering new restaurants and cafes (open to recs!).
+            </h3>
+            <div className="Music">
+              
+            </div>
+            <iframe src="https://open.spotify.com/embed/track/2fhOljbX79loRcdl47SFye?utm_source=generator&theme=0" 
+            width="100%" height="80" frameBorder="0" allowfullscreen="" 
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,13 @@ import './Nav.css';
 function Nav() {
   return (
       <nav className="Nav">
-        <button className="Nav-button"> Home </button>
-          <div className="Right-buttons">
-            <button className="Nav-button"> Resume </button>
-            <button className="Nav-button"> Contact </button>
+        <a className="Nav-div"> Home </a>
+          <div className="Right-divs">
+            <a className="Nav-div"> Resume </a>
+            <a className="Nav-div"> Contact </a>
+            <a className="Nav-div"> Experience </a>
+            <a className="Nav-div"> Projects </a>
+
             <a className="Nav-icon" href="https://linkedin.com/in/alex-park-"> 
               <img src="linkedin.png"></img>
             </a>
