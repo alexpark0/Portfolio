3d77cc6d5f9f6e65bcb27e64587809f3e34ddfb8
alexpark0 <park.ale@northeastern.edu>
2025-01-31 17:01:43 -0500

    experience (wip)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Nav from './components/Nav';
 import Intro from './components/Intro';
+import Experience from './components/Experience';
+
 import './App.css';
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <div className="App">
       <Nav />
       <Intro />
+      <Experience />
     </div>
   );
 }
